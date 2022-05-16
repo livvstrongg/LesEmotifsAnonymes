a4aefb6d47a26d4b8deecb945a005c5aee3a4615
livvstrongg <livyhawk@me.com>
2022-05-16 14:40:48 -0500

    Working in DessertList.js

--- src/components/DessertList.js
+++ src/components/DessertList.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Desserts from './Desserts'
 
-function DessertList() {
+function DessertList({results}) {
+    let data = [];
+    if (results.data) {
+        data = results.data.Search || [];
+    }
   return (
     <div className="result">
+        {data.map()}
         <Desserts />
     </div>
   )

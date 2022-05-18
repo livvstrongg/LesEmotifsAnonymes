ab5503caa4d5038eadbba23c55a7c6624354ec71
livvstrongg <livyhawk@me.com>
2022-05-18 14:40:08 -0500

    RecipeList Page

--- src/App.js
+++ src/App.js
@@ -4,43 +4,33 @@ import DessertList from './components/DessertList'
 import DessertSearch from './components/DessertSearch'
 import Search from './components/Search'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { response } from 'express'
+import { Route, Routes } from 'react-router-dom'
+import RecipeList from './pages/RecipeList'
+
+
 
 
 const App = () => {
 
-const [recipe, setRecipe] = useState([]);
-const [searchValue, setSearchValue] = useState([]);
 
-// const getRecipeRequest = async (searchValue) => {
-  // const url = `https://api.edamam.com/api/recipes/v2?type=public&q=dessert&app_id=9ffd75f7&app_key=897ff7ef6089d74cdb71e048d9852da0&cuisineType=French&dishType=Desserts&field=label`;
-
-//   const response = await fetch(url);
-//   const responseJson = await response.json();
-
-//   console.log(response)
-
-//   if(responseJson.Search){
-//     setRecipe(responseJson.Search);
-//   } 
-//   console.log(responseJson)
-// };
-
-// useEffect(() => {
-//   getRecipeRequest(searchValue);
-// }, [searchValue]);
 
   return(
-    <div className="container-fluid recipe-app">
+    <main>
+      <Routes>
+        <Route path='/'/>
+        <Route path='/:recipeParam'
+        element={<RecipeList/>} />
+    </Routes>
+    {/* <div className="container-fluid recipe-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
         <DessertSearch search='Movies' />
         </div>
       <div className="row">
       <DessertList recipe={recipe} />
-      <Search searchValue={searchValue} setSearchValue={setSearchValue}/>
+      <Search />
       </div>
-    </div>
-
+    </div> */}
+    </main>
   )
 }
 
---
+++ src/pages/RecipeList.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+
+function RecipeList() {
+    const {recipeParam} = useParams()
+    console.log(recipeParam)
+
+    const [recipe, setRecipe] = useState([]);
+
+const getRecipeRequest = async (recipeParam) => {
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${recipeParam}&app_id=9ffd75f7&app_key=897ff7ef6089d74cdb71e048d9852da0&cuisineType=French&dishType=Desserts&field=image`;
+
+  const response = await fetch(url);
+  const responseJson = await response.json();
+
+  console.log(response)
+
+  if(responseJson.hits){
+    setRecipe(responseJson.hits);
+  } 
+  console.log(responseJson.hits)
+};
+
+useEffect(() => {
+  getRecipeRequest(recipeParam);
+}, [recipeParam]);
+
+const loaded = () => {
+    return recipe.map((r) => (
+            <div key={r.recipe.image} className="d-flex justify-content-start m-3">
+                <img src={r.recipe.image} alt='recipe'></img>
+            </div>
+            ))  
+}
+
+const loading = () => {
+    return <h1>Loading.........</h1>
+}
+
+  return (
+    <div>RecipeList
+        {recipe ? loaded() : loading()}
+    </div>
+  )
+  }
+
+export default RecipeList

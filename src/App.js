e353816038cc95edaa394f754b5e23ad43a3b9e0
livvstrongg <livyhawk@me.com>
2022-05-18 21:12:38 -0500

    Working in App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import { Route, Routes } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
 
-  const url = 'https://api.edamam.com/api/recipes/v2?type=public&q=dessert&app_id=9ffd75f7&app_key=897ff7ef6089d74cdb71e048d9852da0&cuisineType=French&dishType=Desserts&imageSize=REGULAR&field=image';
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios('https://api.edamam.com/api/recipes/v2?type=public&q=dessert&app_id=9ffd75f7&app_key=897ff7ef6089d74cdb71e048d9852da0&cuisineType=French&dishType=Desserts&imageSize=REGULAR&field=image')
+      console.log(result.data)
+    }
+  }, [])
+
 
   return (
     <div className="App">
       <Header />
-      <Routes>
+      {/* <Routes>
         <Route exact path='/' element={<Main url={url} /> } /> 
-      </Routes>
+      </Routes> */}
     </div>
   );
 }

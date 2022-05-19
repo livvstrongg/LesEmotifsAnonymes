ff97bf94b7e957d2e6b749a8e6430defaa49c9ae
livvstrongg <livyhawk@me.com>
2022-05-19 15:19:06 -0500

    Working on Styling

--- src/components/recipes/RecipeItem.js
+++ src/components/recipes/RecipeItem.js
@@ -4,17 +4,22 @@ import style from './Recipe.css';
 const RecipeItem = ({title, calories, image, ingredients}) => {
     calories = calories - (calories%1);
     return(
-        <div id="cards" className={style.recipe}>
-            <h1>{title}</h1>
+<div className='card'>
+    <div className='card-inner'>
+    <div className={style.recipe}>
+            <div className='card-front'>
+            <img className={style.image} src={image} alt=""/>
+            <h1 className='title'>{title}</h1>
             <ol>
                 {ingredients.map(ingredient=>(
                     <li>{ingredient.text}</li>
                 ))}
             </ol>
             <p>Calories: {calories}</p>
-            <img className={style.image} src={image} alt=""/>
-
-        </div>
+            </div>
+         </div>
+    </div>
+</div>      
 
     );
 }

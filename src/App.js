0647d85fdcc7f9166262a8b968a6931ef2bf5477
livvstrongg <livyhawk@me.com>
2022-05-18 22:26:45 -0500

    Attempting to implement search bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import RecipeGrid from './components/recipes/RecipeGrid'
+import Search from './components/Search'
 import { Route, Routes } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
@@ -10,10 +11,11 @@ function App() {
 
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios('https://api.edamam.com/api/recipes/v2?type=public&q=dessert&app_id=9ffd75f7&app_key=897ff7ef6089d74cdb71e048d9852da0&cuisineType=French&dishType=Desserts&imageSize=REGULAR&field=label')
+      const result = await axios(`https://api.edamam.com/api/recipes/v2?label=${query}type=public&q=dessert&app_id=9ffd75f7&app_key=897ff7ef6089d74cdb71e048d9852da0&cuisineType=French&dishType=Desserts&imageSize=REGULAR&field=label`)
 
       console.log(result.data.hits)
       setItems(result.data.hits)
@@ -22,12 +24,13 @@ function App() {
 
     fetchItems();
 
-  }, [])
+  }, [query])
 
 
   return (
     <div className="App">
       <Header />
+      <Search getQuery={(q) => setQuery(q)} />
       <RecipeGrid isLoading={isLoading} items={items}/>
       {/* <Routes>
         <Route exact path='/' element={<Main url={url} /> } /> 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react'
 
-const Search = () => {
+const Search = ({getQuery}) => {
+    const [text, setText] = useState('')
+
+    const onChange = (q) => {
+        setText(q)
+        getQuery(q)
+    }
+
   return (
     <div>
         <section className="search">
             <form>
-                <input type='text className='form-control'/>
+                <input type='text' className='form-control' placeholder='Search Dessert' value={text} onChange={(e) => onChange(e.target.value)} autoFocus/>
             </form>
         </section>
     </div>
